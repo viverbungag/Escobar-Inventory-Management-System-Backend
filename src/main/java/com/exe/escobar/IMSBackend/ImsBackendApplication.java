1e2ea13360137722af599c8268e4605c6162aec6
viver <viverbungag1@gmail.com>
2022-07-18 19:56:02 +0800

    Initializing gradle project

---
+++ src/main/java/com/exe/escobar/IMSBackend/ImsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.exe.escobar.IMSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe/escobar/IMSBackend/ImsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.escobar.IMSBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
